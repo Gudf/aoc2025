82658d13239f1b09ab3ec543416e0d8f3ec7981a
Gudf <28691694+Gudf@users.noreply.github.com>
2025-12-01 21:05:11 +0100

    Reorganize input files

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
 open Aoc
 
-let () = Printf.printf "Day 1, part 1: %i\n" (Day01.part1 "inputs/input")
-let () = Printf.printf "Day 1, part 2: %i\n" (Day01.part2 "inputs/input")
+let () = Printf.printf "Day 1, part 1: %i\n" (Day01.part1 "inputs/day01")
+let () = Printf.printf "Day 1, part 2: %i\n" (Day01.part2 "inputs/day01")
